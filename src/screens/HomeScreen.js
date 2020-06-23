ff6046caf886b109390752f25bbc5422ec54bf2d
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-23 00:22:33 -0700

    set background image

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, ImageBackground, StyleSheet } from 'react-native';
 import BodyText from '../components/BodyText';
 import HeadingText from '../components/TitleText';
 import Colors from '../constants/Colors';
@@ -8,8 +8,15 @@ const HomeScreen = () => {
   return (
     <View style={styles.screen}>
       <View style={styles.titleContainer}>
-        <BodyText style={styles.bodyText}>find your</BodyText>
-        <HeadingText style={styles.titleText}>Safe Place</HeadingText>
+        <ImageBackground 
+          source={require('../../assets/shapes/white-concave-shape-short.png')} 
+          style={styles.image}
+        >
+          <View style={styles.textContainer}>
+            <BodyText style={styles.bodyText}>find your</BodyText>
+            <HeadingText style={styles.titleText}>Safe Place</HeadingText>
+          </View>
+        </ImageBackground>
       </View>
       <View style={styles.contentContainer}>
         <View style={styles.content}>
@@ -26,38 +33,49 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: Colors.primaryColor
   },
   titleContainer: {
-    flex: 3,
-    justifyContent: 'flex-end',
+    flex: 2,
+    width: '100%',
+    height: '100%'
+  },
+  image: {
+    flex: 1,
     alignItems: 'center',
-    paddingVertical: 50
+    justifyContent: 'flex-end',
+    resizeMode: 'contain',
+    paddingBottom: '25%'
+  },
+  textContainer: {
+    alignItems: 'center'
   },
   bodyText: {
-    fontSize: 20
+    fontSize: 22
   },
   titleText: {
     letterSpacing: 1.5
   },
   contentContainer: {
-    flex: 2,
-    width: '100%',
-    backgroundColor: Colors.primaryColor
+    flex: 1,
+    width: '100%'
   },
   content: {
-    padding: 20,
+    paddingHorizontal: 30,
+    paddingTop: 15
   },
   card: {
-    width: 160,
-    height: 120,
+    width: 180,
+    height: 100,
     padding: 10,
     borderRadius: 10,
     backgroundColor: 'white'
   },
   whiteBodyText: {
     color: 'white',
-    fontSize: 20
+    fontSize: 22,
+    paddingBottom: 10
   }
 });
 
