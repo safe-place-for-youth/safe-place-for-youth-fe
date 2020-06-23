41e005c8b891fbc2880b2535f42f68ceca292168
Jenna Goldman <jenna.goldman89@gmail.com>
2020-06-23 13:15:17 -0700

    add marker to mapview

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 const MapScreen = () => {
   return (
     <View style={styles.screen}>
       <MapView 
         style={styles.map} 
+        provider='google' 
         initialRegion={{
           latitude: 45.512794,
           longitude: -122.679565,
           latitudeDelta: 0.2,
           longitudeDelta: 0.2,
         }}
-      />
+      >
+        <Marker coordinate={{ latitude: 45.524756, longitude: -122.65703 }}>
+          <MapView.Callout tooltip style={styles.customView}>
+            <View style={styles.calloutText}>
+              <Text>Library</Text>
+            </View>
+          </MapView.Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 };
