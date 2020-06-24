a7e59968baa5bda62577361ad1550b5ec611ee67
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-23 20:07:23 -0700

    designate a color for each category

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -2,3 +2,30 @@ export default {
   primaryColor: '#6280FF',
   accentColor: '#1B26A4'
 }
+
+export const colorCategories = [
+  {
+    category: 'Library',
+    color: '#00C7CC'
+  },
+  {
+    category: 'Homeless Youth Continuum',
+    color: '#FB9961'
+  },
+  {
+    category: 'Homeless Youth Liaisons',
+    color: '#EA7093'
+  },
+  {
+    category: 'Police Station',
+    color: '#4D72A8'
+  },
+  {
+    category: 'Fire Station',
+    color: '#ED0000'
+  },
+  {
+    category: 'Starbucks',
+    color: '#2A764A'
+  }
+]
