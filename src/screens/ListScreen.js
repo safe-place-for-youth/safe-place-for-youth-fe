b53d6ffe4f2341bd9707f354942cf55363ed8e36
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-25 00:59:48 -0700

    filter results with radio buttons

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,33 +1,71 @@
 import React, { useState, useEffect } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
-import { fetchAllPlaces } from '../utils/fetchData';
-import Colors from '../constants/Colors';
+import { View, StyleSheet, TouchableWithoutFeedback, FlatList, TextInput } from 'react-native';
+import BodyText from '../components/BodyText';
 import CardList from '../components/CardList';
+import Colors from '../constants/Colors';
+import { fetchAllPlaces } from '../utils/fetchData';
+import { colorCategories } from'../constants/Colors';
 
 const ListScreen = ({ navigation }) => {
   const [places, setPlaces] = useState([]);
   const [searchInput, setSearchInput] = useState('');
+  const [category, setCategory] = useState('');
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
 
   useEffect(() => {
     fetchAllPlaces()
-      .then(fetchedPlaces => setPlaces(fetchedPlaces));
+      .then(fetchedPlaces => {
+        setPlaces(fetchedPlaces);
+        setFilteredPlaces(fetchedPlaces);
+      });
   }, []);
+
+  useEffect(() => {
+    const filteredElements = places.filter(place => place.category === category);
+    setFilteredPlaces(filteredElements);
+  }, [category])
+
+  const renderFilterButtons = filterData => { 
+    const activeStyle = {...styles.radioButton, backgroundColor: filterData.item.color, color: 'white', borderColor: filterData.item.color};
+   
+    const inactiveStyle = {...styles.radioButton, backgroundColor: 'white', color: filterData.item.color, borderColor: filterData.item.color };
+
+    const radioButtonStyle = (category === filterData.item.category) ? activeStyle : inactiveStyle;
+
+    return (
+    <TouchableWithoutFeedback onPress={() => setCategory(filterData.item.category)}>
+      <View style={styles.radioButtonContainer}>
+        <BodyText style={radioButtonStyle}>{filterData.item.category}</BodyText>
+      </View>
+    </TouchableWithoutFeedback>
+    )
+  };
 
   return (
     <LinearGradient style={styles.screen} colors={[Colors.accentColor, Colors.primaryColor]}>
       <View style={styles.shape}>
-        <TextInput 
-          onChangeText={(newValue) => setSearchInput(newValue)}
-          autoCapitalize='none'
-          autoCorrect={false}
-          value={searchInput}
-          style={styles.searchInput}
-          placeholder='Search'
-        />
+        <View style={styles.searchContainer} >
+          <TextInput 
+            onChangeText={(newValue) => setSearchInput(newValue)}
+            autoCapitalize='none'
+            autoCorrect={false}
+            value={searchInput}
+            style={styles.searchInput}
+            placeholder='Search'
+            placeholderTextColor='white'
+          />
+          <FlatList
+            keyExtractor={item => item.category}
+            data={colorCategories}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            renderItem={renderFilterButtons}
+          />
+        </View>
       </View>
       <View style={styles.list}>
-        <CardList places={places} navigation={navigation} />
+        <CardList places={filteredPlaces} navigation={navigation} />
       </View>
     </LinearGradient>
   );
@@ -37,36 +75,43 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    // backgroundColor: Colors.primaryColor
+    alignItems: 'center'
   },
   shape: {
-    flex: 1.5,
+    flex: 1.9,
     backgroundColor: 'white',
     width: 430,
-    borderBottomLeftRadius: 250,
-    borderBottomRightRadius: 250,
+    borderBottomLeftRadius: 200,
+    borderBottomRightRadius: 200,
     alignItems: 'center',
-    justifyContent: 'center'
+  },
+  searchContainer: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   searchInput: {
     width: '80%',
-    margin: 10,
-    padding: 5,
-    borderWidth: 30,
-    borderColor: 'white',
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+    borderRadius: 5,
     color: 'white',
     backgroundColor: Colors.primaryColor,
-    // shadowColor: Colors.accentColor,
-    // shadowOpacity: 0.8,
-    // shadowRadius: 10,
-    // overflow: 'hidden',
   },
   list: {
     flex: 4,
     height: '100%',
     width: '100%',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginTop: 30
+  },
+  radioButton: {
+    borderWidth: 2,
+    borderRadius: 20, 
+    paddingVertical: 5, 
+    paddingHorizontal: 20,
+    overflow: 'hidden' 
   }
 });
 
