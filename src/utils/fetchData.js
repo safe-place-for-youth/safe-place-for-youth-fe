efc6457e7a51365791dfeae56c6a9daf9ef34441
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-02 12:54:38 -0700

    fetchPlace runs from airtable

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,22 +1,10 @@
-import { API_URL, API_KEY } from 'react-native-dotenv';
+import { API_URL, API_KEY, AT_URL } from 'react-native-dotenv';
 import { colorCategories } from '../constants/Colors';
-
-// export const fetchPlace = id => {
-//   return fetch(`${API_URL}/api/v1/places/${id}`)
-//     .then(res => res.json())
-//     .then(res => console.log(res, 'places'))
-//     .then(place => {
-//       const colorObj = colorCategories.find(obj => obj.category === place.category);
-//       return {
-//         ...place,
-//         color: colorObj.color
-//       }
-//     })
-//     .catch((error) => {console.error(error)});
-// };
+const Airtable = require('airtable');
+const base = new Airtable({apiKey: `${API_KEY}`}).base('appm2DPTBMOxDyjCJ');
 
 export const fetchAllPlaces = () => {
-  return fetch(`https://api.airtable.com/v0/appm2DPTBMOxDyjCJ/Safe%20Places?api_key=${API_KEY}`)
+  return fetch(`${AT_URL}api_key=${API_KEY}`)
     .then(res => res.json())
     .then(res => res.records)
     .then(places => places.map(place => {
@@ -31,10 +19,7 @@ export const fetchAllPlaces = () => {
 };
 
 export const fetchPlace = id => {
-  const Airtable = require('airtable');
-  const base = new Airtable({apiKey: `${API_KEY}`}).base('appm2DPTBMOxDyjCJ');
-
-  const place = base('Safe Places').find(`${id}`, function(err, record) {
+  const place = base('Safe Places').find(id, function(err, record) {
     if (err) { console.error(err); return; }
     console.log('Retrieved', record._rawJson.fields);
 
@@ -44,16 +29,5 @@ export const fetchPlace = id => {
     ...place.rawJson.fields,
     color: colorObj.color
   }
-
-});
-  // return fetch(`${API_URL}/api/v1/places/${id}`)
-  //   .then(res => res.json())
-  //   .then(place => {
-  //     const colorObj = colorCategories.find(obj => obj.category === place.category);
-  //     return {
-  //       ...place,
-  //       color: colorObj.color
-  //     }
-  //   })
-  //   .catch((error) => {console.error(error)});
+  });
 };
