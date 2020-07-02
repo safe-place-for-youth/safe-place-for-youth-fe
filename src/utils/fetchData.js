514b78cb7fefd9d4da97b5a7a41eeebac86be33e
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-02 10:18:58 -0700

    change expo-linear-gradient to earlier version for compatibility

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,8 +1,34 @@
-import { API_URL } from 'react-native-dotenv';
+import { API_KEY } from 'react-native-dotenv';
 import { colorCategories } from '../constants/Colors';
 
+// export const fetchAllPlaces = () => {
+//   return fetch(`${API_URL}/api/v1/places`)
+//     .then(res => res.json())
+//     .then(places => places.map(place => {
+//       const colorObj = colorCategories.find(obj => obj.category === place.category);
+//       return {
+//         ...place,
+//         color: colorObj.color
+//       }
+//     }))
+//     .catch((error) => {console.error(error)});
+// };
+
+// export const fetchPlace = id => {
+//   return fetch(`${API_URL}/api/v1/places/${id}`)
+//     .then(res => res.json())
+//     .then(place => {
+//       const colorObj = colorCategories.find(obj => obj.category === place.category);
+//       return {
+//         ...place,
+//         color: colorObj.color
+//       }
+//     })
+//     .catch((error) => {console.error(error)});
+// };
+
 export const fetchAllPlaces = () => {
-  return fetch(`${API_URL}/api/v1/places`)
+  return fetch(`${API_KEY}`)
     .then(res => res.json())
     .then(places => places.map(place => {
       const colorObj = colorCategories.find(obj => obj.category === place.category);
