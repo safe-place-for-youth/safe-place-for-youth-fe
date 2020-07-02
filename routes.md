60c3d92f21cfea4c517c4733e5703662a96c5eb7
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-02 12:00:22 -0700

    get all places fetch runs from airtable

--- routes.md
+++ routes.md
@@ -1,3 +1,3 @@
-GET all places from MongoDB:
+GET all places from MongoDB: `${API_URL}/api/v1/places`
 
-GET all places from AirTable: https://api.airtable.com/v0/appm2DPTBMOxDyjCJ/Safe%20Places?api_key=keyyI51LEef1wKIFI
+GET all places from AirTable: 'https://api.airtable.com/v0/appm2DPTBMOxDyjCJ/Safe%20Places?api_key=keyyI51LEef1wKIFI
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,22 +1,10 @@
-import { API_KEY } from 'react-native-dotenv';
+import { API_URL, API_KEY } from 'react-native-dotenv';
 import { colorCategories } from '../constants/Colors';
-
-// export const fetchAllPlaces = () => {
-//   return fetch(`${API_URL}/api/v1/places`)
-//     .then(res => res.json())
-//     .then(places => places.map(place => {
-//       const colorObj = colorCategories.find(obj => obj.category === place.category);
-//       return {
-//         ...place,
-//         color: colorObj.color
-//       }
-//     }))
-//     .catch((error) => {console.error(error)});
-// };
 
 // export const fetchPlace = id => {
 //   return fetch(`${API_URL}/api/v1/places/${id}`)
 //     .then(res => res.json())
+//     .then(res => console.log(res, 'places'))
 //     .then(place => {
 //       const colorObj = colorCategories.find(obj => obj.category === place.category);
 //       return {
@@ -28,27 +16,44 @@ import { colorCategories } from '../constants/Colors';
 // };
 
 export const fetchAllPlaces = () => {
-  return fetch(`${API_KEY}`)
+  return fetch(`https://api.airtable.com/v0/appm2DPTBMOxDyjCJ/Safe%20Places?api_key=${API_KEY}`)
     .then(res => res.json())
+    .then(res => res.records)
     .then(places => places.map(place => {
-      const colorObj = colorCategories.find(obj => obj.category === place.category);
+      const colorObj = colorCategories.find(obj => obj.category === place.fields.category);
       return {
-        ...place,
+        _id: place.id,
+        ...place.fields,
         color: colorObj.color
-      }
+      };
     }))
     .catch((error) => {console.error(error)});
 };
 
 export const fetchPlace = id => {
-  return fetch(`${API_URL}/api/v1/places/${id}`)
-    .then(res => res.json())
-    .then(place => {
-      const colorObj = colorCategories.find(obj => obj.category === place.category);
-      return {
-        ...place,
-        color: colorObj.color
-      }
-    })
-    .catch((error) => {console.error(error)});
+  const Airtable = require('airtable');
+  const base = new Airtable({apiKey: `${API_KEY}`}).base('appm2DPTBMOxDyjCJ');
+
+  const place = base('Safe Places').find(`${id}`, function(err, record) {
+    if (err) { console.error(err); return; }
+    console.log('Retrieved', record._rawJson.fields);
+
+  const colorObj = colorCategories.find(obj => obj.category === place.rawJson.fields.category);
+
+  return {
+    ...place.rawJson.fields,
+    color: colorObj.color
+  }
+
+});
+  // return fetch(`${API_URL}/api/v1/places/${id}`)
+  //   .then(res => res.json())
+  //   .then(place => {
+  //     const colorObj = colorCategories.find(obj => obj.category === place.category);
+  //     return {
+  //       ...place,
+  //       color: colorObj.color
+  //     }
+  //   })
+  //   .catch((error) => {console.error(error)});
 };
