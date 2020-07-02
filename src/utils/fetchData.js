6ff09e0c83a3003b0aa072bac211ffc849ada976
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-02 13:43:50 -0700

    both airtable fetches set up as per AT documentation

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,10 +1,35 @@
 import { API_URL, API_KEY, AT_URL } from 'react-native-dotenv';
 import { colorCategories } from '../constants/Colors';
-const Airtable = require('airtable');
+import Airtable from 'airtable';
 const base = new Airtable({apiKey: `${API_KEY}`}).base('appm2DPTBMOxDyjCJ');
 
+// export const fetchAllPlaces = () => {
+//   return base('Safe Places').select({
+//     // Selecting the first 3 records in Grid view:
+//     maxRecords: 25,
+//     view: "Grid view"
+//   }).eachPage(function page(records, fetchNextPage) {
+//     // This function (`page`) will get called for each page of records.
+//     records.map(place => {
+//       const colorObj = colorCategories.find(obj => obj.category === place._rawJson.fields.category);
+//       // console.log(place._rawJson.fields);
+//       return {
+//         ...place._rawJson.fields,
+//         color: colorObj.color
+//       };
+//     });
+//     // To fetch the next page of records, call `fetchNextPage`.
+//     // If there are more records, `page` will get called again.
+//     // If there are no more records, `done` will get called.
+//     fetchNextPage();
+
+//   }, function done(err) {
+//       if (err) { console.error(err); return; }
+//   });
+// };
+
 export const fetchAllPlaces = () => {
-  return fetch(`${AT_URL}api_key=${API_KEY}`)
+  return fetch(`https://api.airtable.com/v0/appm2DPTBMOxDyjCJ/Safe%20Places?api_key=${API_KEY}`)
     .then(res => res.json())
     .then(res => res.records)
     .then(places => places.map(place => {
@@ -12,7 +37,7 @@ export const fetchAllPlaces = () => {
       return {
         _id: place.id,
         ...place.fields,
-        color: colorObj.color
+        color: colorObj?.color
       };
     }))
     .catch((error) => {console.error(error)});
@@ -21,13 +46,15 @@ export const fetchAllPlaces = () => {
 export const fetchPlace = id => {
   const place = base('Safe Places').find(id, function(err, record) {
     if (err) { console.error(err); return; }
-    console.log('Retrieved', record._rawJson.fields);
+    console.log('Retrieved', record?._rawJson.fields);
 
-  const colorObj = colorCategories.find(obj => obj.category === place.rawJson.fields.category);
+  const colorObj = colorCategories.find(obj => obj.category === place?._rawJson.fields.category);
 
   return {
-    ...place.rawJson.fields,
-    color: colorObj.color
+    ...place?._rawJson.fields,
+    color: colorObj?.color
   }
   });
+
+  return place;
 };
