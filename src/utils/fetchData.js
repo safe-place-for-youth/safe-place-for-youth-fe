4a2415485273642c31d0e400c5dac43db34d384d
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-02 18:55:10 -0700

    clean up comments in fetchData

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -19,45 +19,6 @@ export const fetchAllPlaces = async() => {
   })
 }
 
-// export const fetchAllPlaces = async() => {
-//   const places = await base('Safe Places').select({
-//     // Selecting the first 3 records in Grid view:
-//     maxRecords: 25,
-//     view: "Grid view"
-//   }).eachPage(function page(records, fetchNextPage) {
-//     // This function (`page`) will get called for each page of records.
-//     // To fetch the next page of records, call `fetchNextPage`.
-//     // If there are more records, `page` will get called again.
-//     // If there are no more records, `done` will get called.
-//     records.map(place => {
-//       const colorObj = colorCategories.find(obj => obj.category === place._rawJson.fields.category);
-      
-//       return {
-//         ...place._rawJson.fields,
-//         color: colorObj.color
-//       };
-//     });
-//     fetchNextPage();
-//   });
-//   console.log(places, 'outside places');
-//   return places;
-// };
-
-// export const fetchAllPlaces = () => {
-//   return fetch(`https://api.airtable.com/v0/appm2DPTBMOxDyjCJ/Safe%20Places?api_key=${API_KEY}`)
-//     .then(res => res.json())
-//     .then(res => res.records)
-//     .then(places => places.map(place => {
-//       const colorObj = colorCategories.find(obj => obj.category === place.fields.category);
-//       return {
-//         _id: place.id,
-//         ...place.fields,
-//         color: colorObj?.color
-//       };
-//     }))
-//     .catch((error) => {console.error(error)});
-// };
-
 export const fetchPlace = async(id) => {
   const place = await base('Safe Places').find(id);
 
