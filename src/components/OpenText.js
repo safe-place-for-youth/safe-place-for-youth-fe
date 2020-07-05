351d57040fd6a1f9f3ff0af442dd49073a8a94ac
Dannie Schumaker <draeschumaker@gmail.com>
2020-07-05 15:12:16 -0700

    time comparison algorithm changes open status and closing time

--- src/components/OpenText.js
+++ src/components/OpenText.js
@@ -3,13 +3,14 @@ import { StyleSheet } from 'react-native';
 import BodyText from './BodyText';
 
 const OpenText = ({ closingTime }) => {
-  let hours = closingTime.slice(0, 2);
-  const minutes = closingTime.slice(2, 4);
-  const amOrPm = (hours >= 12) ? 'pm' : 'am';
-  hours = (hours % 12) || 12;
-  const parsedTime = `${hours}:${minutes} ${amOrPm}`;
+  // let hours = closingTime.slice(0, 2);
+  // const minutes = closingTime.slice(2, 4);
+  // const amOrPm = (hours >= 12) ? 'pm' : 'am';
+  // hours = (hours % 12) || 12;
+  // const parsedTime = `${hours}:${minutes} ${amOrPm}`;
 
-  return <BodyText><BodyText style={styles.openText}>Open</BodyText> until {parsedTime}</BodyText>
+  return <BodyText><BodyText style={styles.openText}>Open</BodyText> until {closingTime}</BodyText>
+  // return <BodyText><BodyText style={styles.openText}>Open</BodyText> until {parsedTime}</BodyText>
 };
 
 const styles = StyleSheet.create({
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -15,14 +15,97 @@ import { fetchAllPlaces } from '../utils/fetchData';
 
 const HomeScreen = ({ navigation }) => {
   const [places, setPlaces] = useState([]);
+  const [isOpen, setIsOpen] = useState(null);
 
   useEffect(() => {
     fetchAllPlaces()
       .then(fetchedPlaces => fetchedPlaces.slice(0, 5))
       .then(nearestPlaces => setPlaces(nearestPlaces));
   }, []);
+
+  // get current date
+  const currentDate = new Date()
+  // store current date in new variable
+  const today = currentDate;
+  // get current time in ms
+  const currentTime = today.getTime();
+  // get current day of week as number (0-6)
+  const dayOfWeekAsNumber = today.getDay();
+  // convert to day (Sunday - Saturday)
+  const weekdayFactory = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday'
+  }
+
+  const dayOfWeekAsString = weekdayFactory[dayOfWeekAsNumber];
+  const dayOfWeekLowerCase = dayOfWeekAsString.toLowerCase();
+
+  // get string that matches column in data
+  const openHoursRequest = `${dayOfWeekLowerCase}_open`;
+  const closeHoursRequest = `${dayOfWeekLowerCase}_close`;
   
-  const renderPlaceCard = placeData => (
+  const renderPlaceCard = placeData => {
+    console.log(placeData.item.hours)
+
+    const openingTimeData = '9:00 am';
+    const closingTimeData = '5:00 pm';
+    /*
+    // find opening hours for dayOfWeekAsString
+    const openingData = placeData.item.hours[openHoursRequest];
+    // find closing hours for that day
+    const closingData = placeData.item.hours[closeHoursRequest];
+*/
+
+    // get hours and minutes
+    const getHoursAndMinutes = timeData => {
+      if(timeData === 'closed') {
+        const hours = 0;
+        const minutes = 0;
+        return {hours, minutes};
+      }
+
+      const [time, modifier] = timeData.split(' ');
+      const [stringHours, stringMinutes] = time.split(':');
+      
+      // address modifier deviations (punctuation, capitalization)
+      
+      let hours = parseInt(stringHours, 10);
+      const minutes = parseInt(stringMinutes, 10);
+
+      if(hours === 12 && modifier === 'am') {
+        hours = 0;
+      };
+      
+      if(modifier === 'pm') {
+        hours = hours + 12;
+      };
+
+      return {hours, minutes};
+    };
+
+    const { hours: openingHours, minutes: openingMinutes } = getHoursAndMinutes(openingTimeData);
+
+    const {hours: closingHours, minutes: closingMinutes} = getHoursAndMinutes(closingTimeData);
+  
+    // set opening and closing times for current day in ms
+    const openingTimeToday = new Date();
+    openingTimeToday.setHours(openingHours, openingMinutes);
+    
+    const closingTimeToday = new Date();
+    closingTimeToday.setHours(closingHours, closingMinutes);
+
+    // compare current time to opening and closing times
+    const openStatus = currentTime >= openingTimeToday && currentTime < closingTimeToday ? true : false;
+    
+    // if open set open state and render closing time in original string format
+    setIsOpen(openStatus);
+    
+    return (
     <TouchableOpacity
       activeOpacity={0.7}
       onPress={() => {
@@ -34,11 +117,11 @@ const HomeScreen = ({ navigation }) => {
       >
         <Card 
           placeName={placeData.item.name} 
-          isOpen={true}
-          closingTime='1700'
+          isOpen={isOpen}
+          closingTime={closingTimeData}
         />
       </TouchableOpacity>
-  );
+  )};
 
   return (
     <View style={styles.screen}>
