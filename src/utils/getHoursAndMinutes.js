cfb838535f8b2dd50826c02a0bb8ae11757ca786
Dannie Schumaker <draeschumaker@gmail.com>
2020-07-07 01:06:11 -0700

    handle edge cases in getHoursAndMinutes

--- src/utils/getHoursAndMinutes.js
+++ src/utils/getHoursAndMinutes.js
@@ -1,5 +1,5 @@
 export const getHoursAndMinutes = timeData => {
-  if(timeData === 'closed') {
+  if(!timeData || timeData.toLowerCase() === 'closed' || timeData.toLowerCase() === 'n/a') {
     const hours = 0;
     const minutes = 0;
     return { hours, minutes };
@@ -7,17 +7,15 @@ export const getHoursAndMinutes = timeData => {
 
   const [time, modifier] = timeData.split(' ');
   const [stringHours, stringMinutes] = time.split(':');
-  
-  // handle edge cases (punctuation, capitalization, empty fields...)
-  
+    
   let hours = parseInt(stringHours, 10);
   const minutes = parseInt(stringMinutes, 10);
 
-  if(hours === 12 && modifier === 'am') {
+  const modifierWithoutPunctuation = modifier.toLowerCase().replace(/./g, '');
+  if(hours === 12 && modifierWithoutPunctuation === 'am') {
     hours = 0;
   };
-  
-  if(modifier === 'pm') {
+  if(modifierWithoutPunctuation === 'pm') {
     hours = hours + 12;
   };
 
