4a202fb712533747fdb16f916b3f167ebcab9ba6
Dannie Schumaker <draeschumaker@gmail.com>
2020-07-06 22:42:55 -0700

    use dynamic data in time comparison algorithm

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -15,7 +15,6 @@ import { fetchAllPlaces } from '../utils/fetchData';
 
 const HomeScreen = ({ navigation }) => {
   const [places, setPlaces] = useState([]);
-  const [isOpen, setIsOpen] = useState(null);
 
   useEffect(() => {
     fetchAllPlaces()
@@ -29,6 +28,7 @@ const HomeScreen = ({ navigation }) => {
   const today = currentDate;
   // get current time in ms
   const currentTime = today.getTime();
+ 
   // get current day of week as number (0-6)
   const dayOfWeekAsNumber = today.getDay();
   // convert to day (Sunday - Saturday)
@@ -46,33 +46,25 @@ const HomeScreen = ({ navigation }) => {
   const dayOfWeekLowerCase = dayOfWeekAsString.toLowerCase();
 
   // get string that matches column in data
-  const openHoursRequest = `${dayOfWeekLowerCase}_open`;
-  const closeHoursRequest = `${dayOfWeekLowerCase}_close`;
+  const openingHoursRecord = `${dayOfWeekLowerCase}_open`;
+  const closingHoursRecord = `${dayOfWeekLowerCase}_close`;
   
   const renderPlaceCard = placeData => {
-    console.log(placeData.item.hours)
-
-    const openingTimeData = '9:00 am';
-    const closingTimeData = '5:00 pm';
-    /*
-    // find opening hours for dayOfWeekAsString
-    const openingData = placeData.item.hours[openHoursRequest];
-    // find closing hours for that day
-    const closingData = placeData.item.hours[closeHoursRequest];
-*/
-
-    // get hours and minutes
+    const openingTimeData = placeData.item[openingHoursRecord];
+    const closingTimeData = placeData.item[closingHoursRecord];
+    
+      // get hours and minutes
     const getHoursAndMinutes = timeData => {
       if(timeData === 'closed') {
         const hours = 0;
         const minutes = 0;
-        return {hours, minutes};
+        return { hours, minutes };
       }
 
       const [time, modifier] = timeData.split(' ');
       const [stringHours, stringMinutes] = time.split(':');
       
-      // address modifier deviations (punctuation, capitalization)
+      // handle edge cases (punctuation, capitalization, empty fields...)
       
       let hours = parseInt(stringHours, 10);
       const minutes = parseInt(stringMinutes, 10);
@@ -89,8 +81,7 @@ const HomeScreen = ({ navigation }) => {
     };
 
     const { hours: openingHours, minutes: openingMinutes } = getHoursAndMinutes(openingTimeData);
-
-    const {hours: closingHours, minutes: closingMinutes} = getHoursAndMinutes(closingTimeData);
+    const { hours: closingHours, minutes: closingMinutes } = getHoursAndMinutes(closingTimeData);
   
     // set opening and closing times for current day in ms
     const openingTimeToday = new Date();
@@ -100,21 +91,18 @@ const HomeScreen = ({ navigation }) => {
     closingTimeToday.setHours(closingHours, closingMinutes);
 
     // compare current time to opening and closing times
-    const openStatus = currentTime >= openingTimeToday && currentTime < closingTimeToday ? true : false;
-    
-    // if open set open state and render closing time in original string format
-    setIsOpen(openStatus);
+    const isOpen = currentTime >= openingTimeToday && currentTime < closingTimeToday ? true : false;
     
     return (
-    <TouchableOpacity
-      activeOpacity={0.7}
-      onPress={() => {
-        navigation.navigate({ routeName: 'Detail', params: {
-          placeId: placeData.item._id
-        }});
-      }}
-      style={{...styles.card, marginLeft: placeData.index === 0 ? 20 : 0}}
-      >
+      <TouchableOpacity
+        activeOpacity={0.7}
+        onPress={() => {
+          navigation.navigate({ routeName: 'Detail', params: {
+            placeId: placeData.item._id
+          }});
+        }}
+        style={{ ...styles.card, marginLeft: placeData.index === 0 ? 20 : 0 }}
+        >
         <Card 
           placeName={placeData.item.name} 
           isOpen={isOpen}
@@ -138,7 +126,7 @@ const HomeScreen = ({ navigation }) => {
       </View>
       <View style={styles.contentContainer}>
         <View style={styles.listContainer}>
-          <BodyText style={{...styles.bodyText, color: 'white', marginLeft: 20 }}>nearby</BodyText>
+          <BodyText style={{ ...styles.bodyText, color: 'white', marginLeft: 20 }}>nearby</BodyText>
           <FlatList
             keyExtractor={item => item._id}
             data={places}
